c9ea18b8318a8ad64f52bfca7501b35721607263
Flyers2 <Nizukan@yahoo.com>
2014-11-03 15:08:35 -0500

    adding backend

---
+++ retrieveNotes.php
@@ -0,0 +1,19 @@
+<?php
+class Retrieve_notes{
+    function querydb(){
+        $email = "baby";//trim($_POST["email"]);
+        include_once 'database.php';
+        $dbh = $database->create_dbh();
+        /*$query = "SELECT notes.noteID, users.email, notes.note  from notes where '".$email."'=users.email" 
+          ."INNER JOIN users on users.usersID = notes.userID";*/
+        $query = "SELECT note from notes WHERE email = :email";
+        $sth = $dbh->prepare($query);
+        $sth->bindValue("email",$email);
+        $sth->execute();
+        $result = $sth->fetchAll(PDO::FETCH_ASSOC);      
+        $result = json_encode($result);
+        echo $result;
+    }
+}
+$get_notes = new Retrieve_notes();
+$get_notes->querydb();
